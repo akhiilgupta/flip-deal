e30430a937c8d4193f08bf2aa4a2c9c35fe7d49c
akhil2211 <akhil.gupta@meesho.com>
2023-04-07 23:17:58 +0530

    close application after execution

--- src/main/java/com/flipdeal/service/CommandLineAppStartupRunner.java
+++ src/main/java/com/flipdeal/service/CommandLineAppStartupRunner.java
@@ -2,17 +2,24 @@ package com.flipdeal.service;
 
 import com.flipdeal.service.service.ProductService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.stereotype.Component;
 
 @Component
+@Slf4j
 @RequiredArgsConstructor
 public class CommandLineAppStartupRunner implements CommandLineRunner {
 
     private final ProductService productService;
 
+    private final ConfigurableApplicationContext context;
+
     @Override
     public void run(String... args) throws Exception {
         productService.createPromotionSet(args[0]);
+        log.info("terminating application after creating output.json file");
+        context.close();
     }
 }
